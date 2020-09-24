c6d623faf59ecec0c1d22908fde7aadcb9d5073c
Jawkaixuan <j_kaixuan@live.com>
2020-09-24 22:32:38 +0800

    Added Donggu Only accept Ace in Spades

--- src/algorithms/checkDonggu.js
+++ src/algorithms/checkDonggu.js
@@ -1,5 +1,5 @@
 export const checkDonggu = cardsSet => {
-    if (cardsSet[3][0] === 1 && cardsSet[4][2] !== "n") {
+    if (cardsSet[3][0] === 1 && cardsSet[3][1] === "s" && cardsSet[4][2] !== "n") {
         return true
     }
     return false
