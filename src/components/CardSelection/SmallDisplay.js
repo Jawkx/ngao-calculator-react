7e01ca255eb9ceb75ea47612a66697af3210d256
Jawkaixuan <j_kaixuan@live.com>
2020-09-25 14:37:00 +0800

    Stylistic Update

--- src/components/CardSelection/SmallDisplay.js
+++ src/components/CardSelection/SmallDisplay.js
@@ -3,7 +3,7 @@ import { GiDiamonds, GiClubs, GiHearts, GiSpades } from 'react-icons/gi'
 
 function SmallDisplay(props) {
     const display = props.cardArr.map(item =>
-        <div classname="small-display-item">
+        <div className="small-display-item">
             <h1> {item[2] === "n" ? item[0] : item[2].toUpperCase()}</h1>
             {item[1] === "d" ? <GiDiamonds style={{ color: "red" }} /> : item[1] === "c" ? <GiClubs /> : item[1] === "h" ? <GiHearts style={{ color: "red" }} /> : item[1] === "s" ? <GiSpades /> : null}
         </div>
