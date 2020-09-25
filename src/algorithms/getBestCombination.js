40d5bea380701f373684f8c99da606c0e8e2a954
Jawkaixuan <j_kaixuan@live.com>
2020-09-25 18:39:48 +0800

    Remove redundant check length function length

--- src/algorithms/getBestCombination.js
+++ src/algorithms/getBestCombination.js
@@ -11,39 +11,33 @@ const getBestCombination = arr => {
     const allPosibility = getPermutation(arr)
     const allPassport = getPassport(allPosibility)
 
-
-
     if (allPassport.length !== 0) {
         for (let i = 0; i < allPassport.length; i++) {
 
-            if (arr.length === 5) {
-                const allPosibility = getPermutation(arr)
-                const allPassport = getPassport(allPosibility)
-                if (findDupes(arr)) {
-                    return [arr, "invalid, dupes."]
-                }
-                if (allPassport.length !== 0) {
-                    for (let i = 0; i < allPassport.length; i++) {
-                        if (checkFullPicture(allPassport[i])) {
-                            return [allPassport[i], "Full Picture"]
-                        }
-
-
-                        if (checkDonggu(allPassport[i])) {
-                            return [allPassport[i], "Dong gu"]
-                        }
-
-                        if (checkBouBou(allPassport[i])) {
-                            return [allPassport[i], "Bou Bou"]
-                        }
+            const allPosibility = getPermutation(arr)
+            const allPassport = getPassport(allPosibility)
+            if (findDupes(arr)) {
+                return [arr, "invalid, dupes."]
+            }
+            if (allPassport.length !== 0) {
+                for (let i = 0; i < allPassport.length; i++) {
+                    if (checkFullPicture(allPassport[i])) {
+                        return [allPassport[i], "Full Picture"]
                     }
 
-                    return (getLargestNumber(allPassport))
-                } else {
-                    return [arr, "No Passport"]
+
+                    if (checkDonggu(allPassport[i])) {
+                        return [allPassport[i], "Dong gu"]
+                    }
+
+                    if (checkBouBou(allPassport[i])) {
+                        return [allPassport[i], "Bou Bou"]
+                    }
                 }
+
+                return (getLargestNumber(allPassport))
             } else {
-                return [arr, "No Enough Card"]
+                return [arr, "No Passport"]
             }
         }
     }
