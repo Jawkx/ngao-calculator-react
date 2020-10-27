1555ae9305ab0ce7ae3f8b36a4a3f9e79fb6ad21
Jawkaixuan <j_kaixuan@live.com>
2020-10-27 14:09:16 +0800

    Fixed bug in getBestCombination()

--- src/algorithms/getBestCombination.js
+++ src/algorithms/getBestCombination.js
@@ -16,30 +16,26 @@ const getBestCombination = arr => {
     if (allPassport.length !== 0) {
         for (let i = 0; i < allPassport.length; i++) {
 
-            const allPosibility = getPermutation(arr)
-            const allPassport = getPassport(allPosibility)
-
-            if (allPassport.length !== 0) {
-                for (let i = 0; i < allPassport.length; i++) {
-                    if (checkFullPicture(allPassport[i])) {
-                        return [allPassport[i], "Full Picture"]
-                    }
-
-
-                    if (checkDonggu(allPassport[i])) {
-                        return [allPassport[i], "Dong gu"]
-                    }
-
-                    if (checkBouBou(allPassport[i])) {
-                        return [allPassport[i], "Bou Bou"]
-                    }
+            for (let i = 0; i < allPassport.length; i++) {
+                if (checkFullPicture(allPassport[i])) {
+                    return [allPassport[i], "Full Picture"]
                 }
 
-                return (getLargestNumber(allPassport))
-            } else {
-                return [arr, "No Passport"]
+
+                if (checkDonggu(allPassport[i])) {
+                    return [allPassport[i], "Dong gu"]
+                }
+
+                if (checkBouBou(allPassport[i])) {
+                    return [allPassport[i], "Bou Bou"]
+                }
             }
+
+            return (getLargestNumber(allPassport))
+
         }
+    } else {
+        return [arr, "No Passport"]
     }
 }
 
