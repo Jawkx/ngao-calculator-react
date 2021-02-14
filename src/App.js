a57f29323139a43f02c956eb8232adb7bbc1ab75
Jawkaixuan <j_kaixuan@live.com>
2021-02-15 04:23:51 +0800

    Finalize everything

--- src/App.js
+++ src/App.js
@@ -1,41 +1,59 @@
-import React, { useState } from 'react';
-import CardSelection from './components/CardSelection/CardSelection'
-import CardDisplay from './components/CardDisplay/CardDisplay'
-import getBestCombination from './algorithms/getBestCombination'
-
+import React, { useState } from "react";
+import CardSelection from "./components/CardSelection/CardSelection";
+import CardDisplay from "./components/CardDisplay/CardDisplay";
+import getBestCombination from "./algorithms/getBestCombination";
+import { FaGithub, FaReact } from "react-icons/fa";
 function App() {
-  const [currentValue, setValue] = useState([])
-  const [answer, setAnswer] = useState("Press Calculate to show best combination")
-  const [calculated, setCalculated] = useState(false)
+  const [currentValue, setValue] = useState([]);
+  const [answer, setAnswer] = useState(
+    "Press Calculate to show best combination"
+  );
+  const [calculated, setCalculated] = useState(false);
   const calculate = () => {
-    let [cardArr, answer] = getBestCombination(currentValue)
-    setValue(cardArr)
-    setAnswer(answer)
-    setCalculated(true)
-  }
+    let [cardArr, answer] = getBestCombination(currentValue);
+    setValue(cardArr);
+    setAnswer(answer);
+    setCalculated(true);
+  };
 
   const reset = () => {
-    setValue([])
-    setAnswer("Press Calculate to show best combination")
-    setCalculated(false)
-  }
+    setValue([]);
+    setAnswer("Press Calculate to show best combination");
+    setCalculated(false);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="app-title"> Ngao Calculator </h1>
+        <h1 className="app-title">
+          <p>Ngao Calculator</p>
+          <FaGithub
+            className="icon"
+            onClick={() =>
+              window.open(
+                "https://github.com/Jawkx/ngao-calculator-react",
+                "_blank"
+              )
+            }
+          />
+          <p className="description">
+            Click me for details <br /> & source
+          </p>
+        </h1>
       </header>
 
       <div className="top-part">
         <CardSelection
-          handleClick={(value) => { setValue(currentValue.concat([value])) }}
+          handleClick={(value) => {
+            setValue(currentValue.concat([value]));
+          }}
           cardSelectionDisplay={currentValue.length < 5}
           currentValue={currentValue}
           reset={() => reset()}
         />
-
       </div>
 
-      <div className="btm-part"
+      <div
+        className="btm-part"
         style={currentValue.length < 5 ? { display: "none" } : null}
       >
         <CardDisplay
@@ -43,13 +61,18 @@ function App() {
           calculated={calculated}
           answer={answer}
         />
-        <h1 className="calculate-button" onClick={() => calculate()}>Calculate</h1>
+        <h1 className="calculate-button" onClick={() => calculate()}>
+          Calculate
+        </h1>
       </div>
       <div className="footer">
-        <h1 className="github-button" onClick={() => window.open("https://github.com/Jawkx/ngao-calculator-react", "_blank")}>Get the source code here</h1>
+        <h1>
+          Made with &#10084; using <FaReact /> js <br />
+          <br />
+          by Jaw
+        </h1>
       </div>
-
-    </div >
+    </div>
   );
 }
 
--- src/components/CardDisplay/CardDisplay.js
+++ src/components/CardDisplay/CardDisplay.js
@@ -1,30 +1,29 @@
-import React from 'react'
-
-import PlayingCardDisplay from './PlayingCardDisplay'
+import React from "react";
+import PlayingCardDisplay from "./PlayingCardDisplay";
 
 function CardDisplay(props) {
-    const cardsArr = props.cardsToDisplay.map(item =>
-        <PlayingCardDisplay
-            key={item[0] + item[1]}
-            number={item[0]}
-            suit={item[1]}
-            type={item[2]}
-        />
-    )
-    const cardsFront = cardsArr.slice(3, 5)
-    const cardsBack = cardsArr.slice(0, 3)
+  const cardsArr = props.cardsToDisplay.map((item) => (
+    <PlayingCardDisplay
+      key={item[0] + item[1]}
+      number={item[0]}
+      suit={item[1]}
+      type={item[2]}
+    />
+  ));
+  const cardsFront = cardsArr.slice(3, 5);
+  const cardsBack = cardsArr.slice(0, 3);
 
-    return (
-        <div className="card-display">
-            <div className="display-card-container playing-card-container">
-                {cardsFront}
-            </div>
-            <div className="display-card-container playing-card-container">
-                {cardsBack}
-            </div>
-            <h1 className="answer">{props.answer}</h1>
-        </div>
-    )
+  return (
+    <div className="card-display">
+      <div className="display-card-container playing-card-container">
+        {cardsFront}
+      </div>
+      <div className="display-card-container playing-card-container">
+        {cardsBack}
+      </div>
+      <h1 className="answer">{props.answer}</h1>
+    </div>
+  );
 }
 
-export default CardDisplay
+export default CardDisplay;
